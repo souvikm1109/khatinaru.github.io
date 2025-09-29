ab04f729f7822a1c3d4952295818ff9d657768e7
souvikm1109 <souvikbaburanaghat@gmail.com>
2025-09-29 14:04:20 +0530

    full

--- components/modals/ProductDetailModal.tsx
+++
@@ -1,5 +0,0 @@
-// This component is no longer in use. 
-// Its functionality has been migrated to the new ProductDetailPage.tsx component 
-// to provide a full-page view instead of a modal.
-// This file can be safely deleted.
-export {};
